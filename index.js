beb7914d149482ec63c72f5f9410a24459271b06
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-06-01 14:32:10 +0700

    penyesuain

---
+++ index.js
@@ -0,0 +1,4 @@
+import './style/style.css';
+import main from './src/main';
+
+main();
---
+++ src/main.js
@@ -0,0 +1,18 @@
+function main() {
+  const getListPertanyaan = () => {
+    // tuliskan kode di sini!
+  };
+
+
+
+  /*
+      jangan ubah kode di bawah ini ya!
+  */
+
+      const renderAllListPertanyaan = (pertanyaan) => 
+
+
+}
+
+
+export default main;
