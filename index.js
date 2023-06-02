98ee8cd9fb54de0ae31a9f82215e2196f8904b9a
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-06-02 22:27:32 +0700

    final template III

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ function getData() {
   // Membuat Permintaan (Request)
   fetch("https://63e706fd9a66df97f012ae2b.mockapi.io/api-frontend-amcc/card", {
     method: "GET", // Menentukan metode GET
-  });
+  })
+    // Mengirim Permintaan (Send Request)
+    .then(function (response) {})
+
+    // Menerima Respons (Receive Response) dan Mengambil Data (Get Data)
+    .then(function (jsonData) {});
+
+    // Penanganan Kesalahan (Error Handling)
+    .catch(function (error){});
 }
 
 getData();
