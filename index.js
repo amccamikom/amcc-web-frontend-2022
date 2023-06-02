9e78f66bc68262733e755e2536a3bbb31f48c4a0
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-06-02 19:05:06 +0700

    finalisasi tahab 2

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ function getData() {
   // Membuat Permintaan (Request)
   fetch("https://63e706fd9a66df97f012ae2b.mockapi.io/api-frontend-amcc/card", {
     method: "GET", // Menentukan metode GET
-  })
+  });
 }
 
-
 getData();
-
-
-
-
